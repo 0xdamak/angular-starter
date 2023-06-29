e5ede1b3c7ac0dc674d0e8e49e8eda99b5528939
Damilola Akinlade <damak.dev@gmail.com>
2023-06-29 09:14:20 +0100

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContributorsComponent } from './components/contributors/contributors.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReposComponent,
+  },
+  {
+    path: 'contributors/:repo',
+    component: ContributorsComponent,
+  },
+  {
+    path: 'user/:name',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
