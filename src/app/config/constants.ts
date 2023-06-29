5a2b091fde98f5e9141a1030a1ad74ba38fe3a98
Damilola Akinlade <damak.dev@gmail.com>
2023-06-29 09:15:52 +0100

    constants

---
+++ src/app/config/constants.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Constants {
+  public static readonly BASE_URL: string = 'https://api.github.com';
+  public static Project_Title: string = 'Angular starter';
+}
